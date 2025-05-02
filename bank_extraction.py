abd67fa0b30fcce791ac8f17504590048651269e
Peter Martian <petmartian@gmail.com>
2025-05-02 16:54:39 +0100

    Update bank_extraction.py

--- bank_extraction.py
+++ bank_extraction.py
@@ -60,7 +60,7 @@ KNOWN_NAMES = [
     "Trzl - Westtech Limited", "Trzl - Willow Commercial Limited", "Trzl - Techcore Limited",
     "Fidelity/Westtech", "Putsherd Integrated", "Beverly Trust",
     "Gilbert", "Rukib Heritage", "Aduroja Temilade",
-    "Telex Charge", "Transfer Charge", "Ocrativane Integrated"
+    "Telex Charge", "Transfer Charge", "Ocrativane Integrated", "WESTTECH:FIDELITY"
 ]
 KNOWN_NAMES_LOWER = [n.lower() for n in KNOWN_NAMES]
 
